3db21c03a761919a2f92f71a5e398bb26a53791a
Anmol Jain UnitresTech <164000233+AjUnitres@users.noreply.github.com>
2024-06-18 19:40:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8081;
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
