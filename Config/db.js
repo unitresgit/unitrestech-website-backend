f92438cd2bc1332d31d2495ee33616ce8035e818
shailesh wagh <shaileshwagh300@gmail.com>
2024-06-20 15:54:56 +0530

    some issue fixed

--- Config/db.js
+++ Config/db.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql2');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+    ssl: 'true' ? {
+    minVersion: 'TLSv1.2',
+    ca: undefined
+    } : null,
 });
 
 connection.connect((err) => {
